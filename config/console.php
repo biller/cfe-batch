ef79f4a8e3f1e411b6ea604d48046c19368d03d2
feder98 <tachirodriguez98@gmail.com>
2018-03-05 17:16:07 -0300

    PR inicial (#1)
    * Esqueleto inicial de proyecto
    
    * Creada accion para emitir comprobantes

---
+++ config/console.php
@@ -0,0 +1,31 @@
+<?php
+
+$params = require(__DIR__ . '/params.php');
+$db = require(__DIR__ . '/db.php');
+
+return [
+    'id' => 'yii2-console-app',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'app\commands',
+    'enableCoreCommands'  => false,
+    'timeZone' => 'America/Argentina/Buenos_Aires', 
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'log' => [
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'schedule' => [
+            'class' => 'omnilight\scheduling\Schedule'
+        ],
+        'db' => $db,
+    ],
+    'params' => $params,
+];
